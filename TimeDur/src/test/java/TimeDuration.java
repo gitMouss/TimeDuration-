9a6386cd17727e844f81ed8dfcc2c0923e3f942c
Moussa <touremouss@hotmail.fr>
2016-04-05 11:24:43 +0200

    TestJava

---
+++ TimeDur/src/test/java/TimeDuration.java
@@ -0,0 +1,15 @@
+package test.java;
+
+import java.util.Date;
+
+public class TimeDuration {
+	
+	public static void main(String[] args){
+		Date d = new Date();
+		
+		System.out.println(d);
+		
+	}
+	
+
+}
